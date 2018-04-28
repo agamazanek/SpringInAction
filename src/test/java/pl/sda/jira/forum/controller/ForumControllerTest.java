d5c18830decb50ce0b14d4b67263f8691b2322b5
ExtremeSoul <clegutko@gmail.com>
2018-04-28 12:51:15 +0200

    Create 1st test class

---
+++ src/test/java/pl/sda/jira/forum/controller/ForumControllerTest.java
@@ -0,0 +1,18 @@
+package pl.sda.jira.forum.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/")
+public class ForumControllerTest {
+
+    @Test
+    public void shouldExistSpaceForum() {
+
+    }
+
+}
+// Czy jest forum, czy jest space
