eb2b1fbe8f05a484e7268a9bedf09b097d66102a
smalaca <ab13.krakow@gmail.com>
2018-06-10 10:02:50 +0200

    PathVariable and RequestMapping default value and name

--- src/main/java/pl/sda/jira/TemplateController.java
+++ src/main/java/pl/sda/jira/TemplateController.java
@@ -1,6 +1,8 @@
 package pl.sda.jira;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,15 @@ public class TemplateController {
     @RequestMapping("/hello-world")
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/hello")
+    public String helloWorld(@RequestParam(defaultValue = "NO NAME", name = "name") String who) {
+        return "Hello " + who + "! Are you lost?";
+    }
+
+    @RequestMapping("/hello-world/{name}")
+    public String helloWorldSomeone(@PathVariable String name) {
+        return "Hello " + name + "! Good to see you again :)";
     }
 }
