b5cb82138088340c5a0c92e307e20a388ba52928
Przemek <fraczek.przemyslaw2@gmail.com>
2018-06-10 10:26:45 +0200

    update

---
+++ src/main/java/pl/sda/jira/documentation/domain/InMemoryDocumentationRepository.java
@@ -0,0 +1,62 @@
+package pl.sda.jira.documentation.domain;
+
+import pl.sda.jira.documentation.domain.exception.DocumentDoestExist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryDocumentationRepository implements DocumentationRepository {
+
+    private List<Documentation> documentations = new ArrayList<>();
+
+    @Override
+    public void add(Documentation documentation) {
+        documentations.add(documentation);
+    }
+
+
+    public boolean exists(Long documentationId) {
+
+        for (Documentation documentation : documentations) {
+            if (documentation.getId() == documentationId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Documentation get(Long documentationId) {
+        if (exists(documentationId)) {
+            return findDocumentation(documentationId);
+        }
+        throw new DocumentDoestExist(documentationId);
+    }
+
+    @Override
+    public void delete(Long documentationId) {
+        if(exists(documentationId)){
+            Documentation documentation = findDocumentation(documentationId);
+            documentations.remove(documentation);
+        }else {
+            throw new DocumentDoestExist(documentationId);
+        }
+    }
+
+    @Override
+    public void update(Documentation documentation ) {
+        delete(documentation.getId());
+        add(documentation);
+    }
+
+    private Documentation findDocumentation(Long documentationId) {
+        Documentation documentation = null;
+        for (int i = 0; i < documentations.size(); i++) {
+            if (documentations.get(i).getId() == documentationId) {
+                documentation = documentations.get(i);
+            }
+
+        }
+        return documentation;
+    }
+}
