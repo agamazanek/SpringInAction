e3fc5031bbdcd6c67fef7a196281b2890e571f21
smalaca <ab13.krakow@gmail.com>
2018-06-10 11:41:53 +0200

    Add System Tests to the TemplateController

--- src/main/java/pl/sda/jira/TemplateApplication.java
+++ src/main/java/pl/sda/jira/TemplateApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TemplateApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TemplateApplication.class);
+        SpringApplication.run(TemplateApplication.class, args);
     }
 }
