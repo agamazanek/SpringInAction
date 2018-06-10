ee5600d3356109e0969a009fd1fad580a8c7a433
agamazanek <aga.a.mazanek@gmail.com>
2018-06-10 15:14:02 +0200

    Add sayHello() and test.

--- src/main/java/pl/sda/jira/calendar/rest/CalendarController.java
+++ src/main/java/pl/sda/jira/calendar/rest/CalendarController.java
@@ -1,17 +1,23 @@
 package pl.sda.jira.calendar.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import pl.sda.jira.calendar.domain.CalendarRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.sda.jira.calendar.domain.service.CalendarService;
 
+@RestController
+@RequestMapping("/calendar")
 public class CalendarController {
-    private CalendarRepository calendarRepository;
+    private CalendarService calendarService;
 
-    @Autowired
-    public CalendarController(CalendarRepository calendarRepository) {
-        this.calendarRepository = calendarRepository;
+
+    public CalendarController(CalendarService calendarService) {
+        this.calendarService = calendarService;
     }
 
-    public boolean existsForPersonWith(String personId) {
-        return calendarRepository.existsForPersonWith(personId);
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return "Hello hello!";
+
     }
+
 }
