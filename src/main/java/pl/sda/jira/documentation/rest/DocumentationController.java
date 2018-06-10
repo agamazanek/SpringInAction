0d4463e1c2629ec794ce3d371a364eaa8d04ff72
mikolajmarek <mikolaj.juras@gmail.com>
2018-06-10 15:32:56 +0200

    Documentation controller w/o tests

--- src/main/java/pl/sda/jira/documentation/rest/DocumentationController.java
+++ src/main/java/pl/sda/jira/documentation/rest/DocumentationController.java
@@ -1,7 +1,49 @@
 package pl.sda.jira.documentation.rest;
 
-import pl.sda.jira.documentation.domain.DocumentationRepository;
+import org.springframework.web.bind.annotation.*;
+import pl.sda.jira.documentation.domain.DocumentationService;
+import pl.sda.jira.documentation.dto.DocumentationDto;
 
+@RestController
+@RequestMapping("document")
 public class DocumentationController {
+
+    private final DocumentationService documentationService;
+
+    public DocumentationController(DocumentationService documentationService) {
+        this.documentationService = documentationService;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.PUT)
+    private String create(@ModelAttribute DocumentationDto documentationDto) {
+        documentationService.add(documentationDto);
+        return "Documentation title: "
+                + documentationDto.getTitle()
+                + " "
+                + documentationDto.getId();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    private String read(@PathVariable Long id) {
+        documentationService.get(id);
+        return "Retrieved: " + id;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.POST)
+    private String update(@ModelAttribute DocumentationDto documentationDto) {
+        documentationService.update(documentationDto.getId(), documentationDto.getTitle());
+        return "Documentation changed: "
+                + documentationDto.getTitle()
+                + " "
+                + documentationDto.getId();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    private String delete(@PathVariable Long id) {
+        documentationService.delete(id);
+        return "Removed: " + id;
+
+    }
+
 
 }
