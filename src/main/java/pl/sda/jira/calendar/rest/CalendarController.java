319e134a94c5bea83006b2080d7aee3204fbb859
agamazanek <aga.a.mazanek@gmail.com>
2018-06-23 11:26:25 +0200

    add service to CalendarController + tests (get, delete - green, put, post - red)

--- src/main/java/pl/sda/jira/calendar/rest/CalendarController.java
+++ src/main/java/pl/sda/jira/calendar/rest/CalendarController.java
@@ -1,5 +1,6 @@
 package pl.sda.jira.calendar.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.sda.jira.calendar.domain.dto.CalendarDto;
 import pl.sda.jira.calendar.domain.model.Calendar;
@@ -10,14 +11,9 @@ import pl.sda.jira.calendar.domain.service.CalendarService;
 public class CalendarController {
     private CalendarService calendarService;
 
-
+    @Autowired
     public CalendarController(CalendarService calendarService) {
         this.calendarService = calendarService;
-    }
-
-    @RequestMapping("/hello")
-    public String sayHello(){
-        return "Hello hello!";
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
@@ -34,7 +30,7 @@ public class CalendarController {
     public void put(@PathVariable String id, @PathVariable CalendarDto name) {
         calendarService.update(id, name);}
 
-    @RequestMapping(path = "/{name}", method = RequestMethod.POST)
+    @RequestMapping(path = "/", method = RequestMethod.POST)
     public String post(@PathVariable CalendarDto calendarDto){
         return calendarService.add(new CalendarDto(CalendarDto.Builder.aCalendar(calendarDto.getName())));
     }
--- src/test/java/pl/sda/jira/calendar/rest/rest/CalendarControllerTest.java
+++ src/test/java/pl/sda/jira/calendar/rest/rest/CalendarControllerTest.java
@@ -1,11 +1,7 @@
 package pl.sda.jira.calendar.rest.rest;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,102 +10,61 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import pl.sda.jira.calendar.domain.dto.CalendarDto;
-import pl.sda.jira.calendar.domain.model.Calendar;
 import pl.sda.jira.calendar.domain.service.CalendarService;
-import pl.sda.jira.calendar.rest.CalendarController;
-
-
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CalendarControllerTest {
-    private static final String NAME = "calendar1";
+
+    private static final String NAME = "calendar9";
+    private static final String NEW_NAME = "myCalendar";
     @Autowired private MockMvc restClient;
-
-
-   @Mock
-   private CalendarService calendarService;
-
-   @InjectMocks
-   private CalendarController calendarController;
-
-   @Before
-   public void setUp() throws Exception {
-       restClient = MockMvcBuilders.standaloneSetup(calendarController).build();
-   }
-
-//    @Test
-//    public void shouldSayHello() throws Exception {
-//        MockHttpServletResponse response = restClient.perform(
-//                MockMvcRequestBuilders.get("/calendar/hello")
-//        ).andReturn().getResponse();
-//
-//        assertEquals(HttpStatus.OK.value(), response.getStatus());
-//        assertEquals("Hello hello!", response.getContentAsString());
-//    }
-
-//    @Test
-//    public void shouldGetCalendar() throws Exception {
-//        MockHttpServletResponse response = restClient.perform(MockMvcRequestBuilders.get("/calendar/1356")).andReturn().getResponse();
-//
-//        assertEquals(HttpStatus.OK.value(), response.getStatus());
-//        assertEquals("1356", response.getContentAsString());
-//
-//    }
+    @Autowired private CalendarService calendarService;
 
     @Test
     public void shouldGetCalendar() throws Exception {
-        MockHttpServletResponse response = restClient.perform(MockMvcRequestBuilders.get("/calendar").param("id", "789")).andReturn().getResponse();
-        String id = "789";
-        Calendar calendar = calendarService.findBy(id);
-        calendarService.add(createCalendarDto());
-        when(calendarService.findBy(id)).thenReturn(calendar);
-        String result = calendar.getId();
+        CalendarDto calendarDto = new CalendarDto(CalendarDto.Builder.aCalendar(NAME));
+        String id = calendarService.add(calendarDto);
+
+        MockHttpServletResponse response = restClient.perform(MockMvcRequestBuilders.get("/calendar/{id}", id)).andReturn().getResponse();
         assertEquals(HttpStatus.OK.value(), response.getStatus());
-        assertEquals(result, response.getContentAsString());
-        verify(calendarService).findBy(id);
+        assertEquals("{\"id\":\"" + id + "\"}", response.getContentAsString());
     }
 
     @Test
     public void shouldDeleteCalendar() throws Exception{
-        MockHttpServletResponse response = restClient.perform(MockMvcRequestBuilders.delete("/calendar/789")).andReturn().getResponse();
-        String id = "789";
-        doNothing().when(calendarService).remove(id);
-        verify(calendarService).remove(id);
+        CalendarDto calendarDto = new CalendarDto(CalendarDto.Builder.aCalendar(NAME));
+        String id = calendarService.add(calendarDto);
+
+        MockHttpServletResponse response = restClient.perform(MockMvcRequestBuilders.delete("/calendar/{id}", id)).andReturn().getResponse();
         assertEquals(HttpStatus.OK.value(), response.getStatus());
-        assertEquals("789", response.getContentAsString());
     }
+
 
     @Test
     public void shouldUpdateCalendar() throws Exception{
-        MockHttpServletResponse response = restClient.perform((MockMvcRequestBuilders.put("/calendar/435/calendar2"))).andReturn().
+
+        CalendarDto calendarDto = new CalendarDto(CalendarDto.Builder.aCalendar(NAME));
+        String id = calendarService.add(calendarDto);
+
+        MockHttpServletResponse response = restClient.perform((MockMvcRequestBuilders.put("/calendar/{id}/{name}", id, NEW_NAME))).andReturn().
                 getResponse();
-        String id = "435";
-        doNothing().when(calendarService).update(id, createCalendarDto());
         assertEquals(HttpStatus.OK.value(), response.getStatus());
-        verify(calendarService).update(id, createCalendarDto());
-        //assertEquals("Updated: 678 to new name: calendar1" , response.getContentAsString());
     }
 
-    private CalendarDto createCalendarDto() {
-       return new CalendarDto(CalendarDto.Builder.aCalendar(NAME));
-    }
 
     @Test
     public void shouldAddCalendar() throws Exception{
-        MockHttpServletResponse response = restClient.perform((MockMvcRequestBuilders.post("/calendar/calendar2"))).andReturn().getResponse();
+        CalendarDto calendarDto = new CalendarDto(CalendarDto.Builder.aCalendar(NAME));
+        String id = calendarService.add(calendarDto);
 
-    assertEquals(HttpStatus.OK.value(), response.getStatus());
-    assertEquals("Added: calendar2", response.getContentAsString());
+        MockHttpServletResponse response = restClient.perform((MockMvcRequestBuilders.post("/calendar/"))
+                .param("calendarDto", calendarDto.getName())).andReturn().getResponse();
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(id, response.getContentAsString());
     }
-
-
-
 }
