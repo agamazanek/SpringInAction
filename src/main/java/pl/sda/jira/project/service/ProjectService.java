c0adb98ca39712b385c60d32cd805060990769bd
martaKopp <kopp.marta@gmail.com>
2018-06-28 21:30:30 +0200

    projectServisTest-all green

--- src/main/java/pl/sda/jira/project/service/ProjectService.java
+++ src/main/java/pl/sda/jira/project/service/ProjectService.java
@@ -1,6 +1,5 @@
 package pl.sda.jira.project.service;
 
-        import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.stereotype.Service;
         import pl.sda.jira.project.domain.Project;
         import pl.sda.jira.project.domain.ProjectAlreadyExistsException;
@@ -8,12 +7,10 @@ package pl.sda.jira.project.service;
         import pl.sda.jira.project.domain.ProjectDto;
         import pl.sda.jira.project.repository.ProjectRepository;
 
-        import java.util.UUID;
-
 @Service
 public class ProjectService {
     private final ProjectRepository repository;
-@Autowired
+
     public ProjectService(ProjectRepository repository) {
         this.repository = repository;
     }
@@ -29,15 +26,11 @@ public class ProjectService {
     public long add(ProjectDto projectDto) {
         if (repository.isExist(projectDto.getName())) throw new ProjectAlreadyExistsException(projectDto.getName());
         else {
-//            Long id=generateId();
             Project project = new Project(projectDto);
             repository.add(project);
             return project.getId();
         }
     }
-//    private static long generateId() {
-//        return UUID.randomUUID().getMostSignificantBits();
-//    }
 
     public void delete(long projectId) {
         if (repository.isExist(projectId)) {
@@ -46,7 +39,6 @@ public class ProjectService {
             throw new ProjectDoesntExistException();
         }
     }
-
 
     public void update(Long id, ProjectDto projectDto) {
         if (repository.isExist(id)) {
