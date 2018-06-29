4b3bb52100517c088032974346457e7b0ecfa3b2
martaKopp <kopp.marta@gmail.com>
2018-06-29 22:29:51 +0200

    projectControllerTest- add two tests

--- src/test/java/pl/sda/jira/project/controler/ProjectControllerTest.java
+++ src/test/java/pl/sda/jira/project/controler/ProjectControllerTest.java
@@ -90,5 +90,29 @@ public class ProjectControllerTest {
         MockHttpServletResponse create = aProjectBy(id);
         assertEquals("{\"name\":\"" + newName + "\"}", create.getContentAsString());
     }
+    @Test
+    public void shouldFailWhenProjectWithSameNameIsAdded() throws Exception{
+        String name = "badName";
 
+        chrome.perform(MockMvcRequestBuilders.put("/project")
+                .param("name",name));
+        MockHttpServletResponse response = chrome.perform(
+                MockMvcRequestBuilders.put("/project")
+                        .param("name",name)
+        ).andReturn().getResponse();
+
+        assertEquals(HttpStatus.NOT_ACCEPTABLE.value(),response.getStatus());
+        assertEquals("badName already exists.",response.getContentAsString());
+    }
+
+    @Test
+    public void shouldFailWhenNonExistentProjectIsDeleted() throws Exception {
+        long incorrectId=666L;
+        MockHttpServletResponse response = chrome.perform(
+                MockMvcRequestBuilders.delete("/project/{incorrectId}",incorrectId)
+        ).andReturn().getResponse();
+
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatus());
+        assertEquals("not found",response.getContentAsString());
+    }
 }
