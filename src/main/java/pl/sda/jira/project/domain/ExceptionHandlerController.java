51c54a05a047c6ee8bb6158db353f6c89f92c611
martaKopp <kopp.marta@gmail.com>
2018-06-30 10:38:52 +0200

    exceptionHandlerController- add forumNotFoundExc and documentNotFoundExc

--- src/main/java/pl/sda/jira/project/domain/ExceptionHandlerController.java
+++ src/main/java/pl/sda/jira/project/domain/ExceptionHandlerController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import pl.sda.jira.documentation.domain.exception.DocumentDoestExist;
+import pl.sda.jira.forum.domain.ForumDoesNotExistExcepton;
 import pl.sda.jira.project.model.ProjectDoesntExistException;
 
 @ControllerAdvice
@@ -14,8 +16,9 @@ public class ExceptionHandlerController {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_ACCEPTABLE);
     }
 
-    @ExceptionHandler(ProjectDoesntExistException.class)
-    public ResponseEntity<String>notFound(ProjectDoesntExistException exception){
+    @ExceptionHandler({ProjectDoesntExistException.class, ForumDoesNotExistExcepton.class,DocumentDoestExist.class})
+    public ResponseEntity<String>notFound(Exception exception){
         return new ResponseEntity<>(exception.getMessage(),HttpStatus.NOT_FOUND);
     }
+
 }
