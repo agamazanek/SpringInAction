ae78d7c9cbe926e0d9c9431e28c439f7bd0f4457
Rosiej <mateusz.rosiejka@gmail.com>
2018-06-30 14:06:20 +0200

    first 3 queries methods with Annotations to project added tests green

--- src/main/java/pl/sda/jira/project/repository/CrudJpaProjectRepository.java
+++ src/main/java/pl/sda/jira/project/repository/CrudJpaProjectRepository.java
@@ -1,9 +1,12 @@
 package pl.sda.jira.project.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.sda.jira.project.model.Project;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +18,13 @@ public interface CrudJpaProjectRepository extends CrudRepository<Project,Long>{
     Project findFirstByName(String name);
 
     Optional<Project> findFirstByAuthorOrName(String author, String name);
+
+    @Query("select p.author from Project p where p.name=:name")
+    List<Project> findAuthorByProjectName(@Param("name") String projectName);
+
+    @Query("select p from Project  p where p.author=:author")
+    List<Project> findProjectsByAuthor(@Param("author") String author);
+
+    @Query("select p from Project p where p.id=:id")
+    Optional<Project> findById(@Param("id") Long id);
 }
