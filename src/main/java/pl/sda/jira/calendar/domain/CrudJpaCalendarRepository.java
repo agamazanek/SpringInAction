14d4425fa026fbbb3d82f8f3c8bcd3070e0ad38f
agamazanek <aga.a.mazanek@gmail.com>
2018-06-30 13:02:45 +0200

    add findBYName() method

--- src/main/java/pl/sda/jira/calendar/domain/CrudJpaCalendarRepository.java
+++ src/main/java/pl/sda/jira/calendar/domain/CrudJpaCalendarRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.sda.jira.calendar.domain.model.Calendar;
 
+import java.util.BitSet;
+import java.util.List;
+
 @Repository
 public interface CrudJpaCalendarRepository extends CrudRepository<Calendar, String>{
 
+    List<Calendar> findByName(String name);
 }
---
+++ src/test/java/pl/sda/jira/calendar/rest/persistency/CrudJpaCalendarRepositoryMethodQueriesTest.java
@@ -0,0 +1,39 @@
+package pl.sda.jira.calendar.rest.persistency;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pl.sda.jira.calendar.domain.CrudJpaCalendarRepository;
+import pl.sda.jira.calendar.domain.model.Calendar;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DataJpaTest
+public class CrudJpaCalendarRepositoryMethodQueriesTest {
+    @Autowired private CrudJpaCalendarRepository repository;
+    @Before
+    public void init(){
+        Calendar calendar = new Calendar("343", "calendar0");
+        Calendar calendar1 = new Calendar("4556", "calendar1");
+        Calendar calendar2 = new Calendar("5765", "calendar3");
+
+        repository.save(calendar);
+        repository.save(calendar1);
+        repository.save(calendar2);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void shouldFindByName() {
+        assertEquals(1, repository.findByName("calendar1").size());
+
+    }
+}
