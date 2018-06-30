9b86ce6c254f65720c87b7d1fba55d1ff9c3bd88
Rosiej <mateusz.rosiejka@gmail.com>
2018-06-30 12:20:23 +0200

    first 3 queries methods to project added

--- src/main/java/pl/sda/jira/project/model/Project.java
+++ src/main/java/pl/sda/jira/project/model/Project.java
@@ -9,13 +9,31 @@ public class Project {
 @Id
 @GeneratedValue
     private Long id;
-    private String name;
+    private String name;;
+    private String author;
 
     public Project(ProjectDto projectDto) {
         this.name=projectDto.getName();
     }
 
     public Project() {
+    }
+
+    public Project(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public Long getId() {
