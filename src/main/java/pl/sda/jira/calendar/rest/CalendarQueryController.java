6b915bf4d0eadd31501dfbac73d200d61dcee1a0
agamazanek <aga.a.mazanek@gmail.com>
2018-07-01 11:20:13 +0200

    add QueryCriteriaDto to queries package

---
+++ src/main/java/pl/sda/jira/calendar/rest/CalendarQueryController.java
@@ -0,0 +1,22 @@
+package pl.sda.jira.calendar.rest;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pl.sda.jira.calendar.queries.QueryCriteriaDto;
+import pl.sda.jira.calendar.domain.service.CalendarQueryService;
+
+@RestController
+@RequestMapping("/calendars")
+public class CalendarQueryController {
+    private CalendarQueryService queryService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String post(@ModelAttribute QueryCriteriaDto queryCriteriaDto) {
+        return  queryCriteriaDto.getName() + queryCriteriaDto.getValue() + queryCriteriaDto.getType();
+    }
+
+
+
+}
