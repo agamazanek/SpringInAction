f927c12c6b4f63dcdac8847ac4525c6d763aa491
smalaca <ab13.krakow@gmail.com>
2018-07-01 11:24:19 +0200

    fix compilation error caused by no class definition

--- src/main/java/pl/sda/jira/calendar/rest/CalendarQueryController.java
+++ src/main/java/pl/sda/jira/calendar/rest/CalendarQueryController.java
@@ -5,18 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sda.jira.calendar.queries.QueryCriteriaDto;
-import pl.sda.jira.calendar.domain.service.CalendarQueryService;
 
 @RestController
 @RequestMapping("/calendars")
 public class CalendarQueryController {
-    private CalendarQueryService queryService;
-
     @RequestMapping(method = RequestMethod.POST)
     public String post(@ModelAttribute QueryCriteriaDto queryCriteriaDto) {
         return  queryCriteriaDto.getName() + queryCriteriaDto.getValue() + queryCriteriaDto.getType();
     }
-
-
-
 }
